649b927f1c4976dd7d3dcc583bec8b90e3c3fb81
enor2017 <enor2017@163.com>
2022-04-28 22:53:14 +0800

    Update: add MainTest to run all test suites.

---
+++ Tests/MainTest.py
@@ -0,0 +1,8 @@
+import unittest
+from DDLTest import DDLTest
+
+"""
+This is the main test file, acting as a driver. It will invoke all other test suites.
+"""
+if __name__ == '__main__':
+    unittest.main()
