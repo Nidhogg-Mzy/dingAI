8154e808abf6b83c154e071eb6976cd0c47da3f8
enor2017 <enor2017@163.com>
2022-04-28 23:02:20 +0800

    Update: Refactor test directory for unittest auto detection.

--- Tests/MainTest.py
+++
@@ -1,8 +0,0 @@
-import unittest
-from DDLTest import DDLTest
-
-"""
-This is the main test file, acting as a driver. It will invoke all other test suites.
-"""
-if __name__ == '__main__':
-    unittest.main()
