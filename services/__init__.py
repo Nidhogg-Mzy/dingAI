a8b688e508603ad30ebfb55ea96688b532035e95
enor2017 <enor2017@163.com>
2023-05-17 05:50:11 -0400

    Update: add SERVICES_MAP in services module.

---
+++ services/__init__.py
@@ -0,0 +1,10 @@
+from .gpt import GPTService
+
+SERVICES_MAP = {
+    'chat': GPTService,
+    'chathistory': GPTService,
+    'chathist': GPTService,
+    'chatload': GPTService,
+    'chatsave': GPTService,
+    'chatdiscard: GPTService',
+}
