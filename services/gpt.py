8374fd382f14ccbb19b3c2981195f6df12e104f5
enor2017 <enor2017@163.com>
2023-05-17 05:14:56 -0400

    Refactor: gpt service implements load_config

--- services/gpt.py
+++ services/gpt.py
@@ -14,46 +14,60 @@ class GPTService(BaseService):
     To use this service, you need to have an API key from OpenAI and provided in
     config['openai']['api_key'].
     """
-    _config = configparser.ConfigParser()
-    _config.read('../config.ini')
-    _OPENAI_CONFIGS = _config['openai']
-    # general
-    _ENABLED = _OPENAI_CONFIGS.getboolean('enabled')
-    _API_KEY = _OPENAI_CONFIGS['api_key']
-    _CACHE_FOLDER = _OPENAI_CONFIGS['cache_folder']
-    os.makedirs(_CACHE_FOLDER, exist_ok=True)
 
-    # chat function
-    _CHAT_ENABLED = _OPENAI_CONFIGS.getboolean('chat_enabled')
-    _CHAT_MODEL = _OPENAI_CONFIGS['chat_model']
+    _initialized = False
 
-    # image function
-    _IMAGE_ENABLED = _OPENAI_CONFIGS.getboolean('image_enabled')
+    _ENABLED = None
+    _API_KEY = None
+    _CACHE_FOLDER = None
+    _CHAT_ENABLED = None
+    _CHAT_MODEL = None
+    _IMAGE_ENABLED = None
 
     @staticmethod
-    def process_query(query: List[str], extra_info: Optional[dict] = None) -> str:
+    def load_config(configs: dict) -> None:
+        # only execute once
+        if GPTService._initialized:
+            return None
+
+        openai_configs = configs['openai']
+        # general
+        GPTService._ENABLED = openai_configs.getboolean('enabled')
+        GPTService._API_KEY = openai_configs['api_key']
+        GPTService._CACHE_FOLDER = openai_configs['cache_folder']
+        os.makedirs(GPTService._CACHE_FOLDER, exist_ok=True)
+
+        # chat function
+        GPTService._CHAT_ENABLED = openai_configs.getboolean('chat_enabled')
+        GPTService._CHAT_MODEL = openai_configs['chat_model']
+
+        # image function
+        GPTService._IMAGE_ENABLED = openai_configs.getboolean('image_enabled')
+
+    @staticmethod
+    def process_query(query: List[str], user_id: str) -> str:
         """
         Process GPT service query.
 
         :param query: a list of strings, the first string must be either 'chat' or 'image',
             the rest of the strings are the prompt for the query.
-        :param extra_info: a dictionary that contains extra information. Possible fields:
-            'user_id' (mandatory): a string, the user id of the user who sent the query.
-            'use_cache' (optional): a boolean, whether to use cache. Default is True. Note that
-                if this is set to False, the cache will be deleted permanently.
-        :return: a string, the response of the query.
+        :param user_id: a string, the user id of the user who sent the query.
+        :return: a string, the response of the query, in Markdown syntax.
         :raises ValueError: if the query is invalid.
         """
+        # must be init before query
+        if not GPTService._initialized:
+            raise ValueError('[Error] not initialized before query, please contact the administrator.')
         # validate query
         if len(query) == 0:
             raise ValueError(f'Invalid query: query length is 0, but is passed into GPTService')
         if query[0] not in ['chat', 'image']:
             raise ValueError(f'Invalid query: query[0] = "{query[0]}", but is passed into GPTService')
-        if extra_info is None or 'user_id' not in extra_info:
-            raise ValueError(f'To use GPTService, "user_id" must be provided in extra_info')
         # if user only pass in func name, but no prompt, return help message
-        if len(query) == 1 or query[1] == 'help':
+        if len(query) == 1:
             return f'[Error] Your query is not complete.\n\n{GPTService.get_help()}'
+        if query[1] == 'help':
+            return GPTService.get_help()
 
         # check if the service is enabled
         if not GPTService._ENABLED:
@@ -71,7 +85,7 @@ class GPTService(BaseService):
         if query[0] == 'chat':
             # check if there is a cache file (json format) for this user,
             # if there is, retrieve the chat history, otherwise, create a new cache file
-            cache_file = f'{GPTService._CACHE_FOLDER}/{extra_info["user_id"]}.json'
+            cache_file = f'{GPTService._CACHE_FOLDER}/{user_id}.json'
             try:
                 with open(cache_file, 'r') as f:
                     chat_history = json.load(f)
@@ -86,6 +100,7 @@ class GPTService(BaseService):
             response = openai.ChatCompletion.create(
                 model=GPTService._CHAT_MODEL,
                 messages=chat_history,
+                max_tokens=500      # max length of generated completion
             )
             # add the response to the chat history
             chat_history.append(response.choices[0].message)
@@ -93,7 +108,7 @@ class GPTService(BaseService):
             with open(cache_file, 'w') as f:
                 json.dump(chat_history, f)
             # return the response
-            return response.choices[0].message
+            return response.choices[0].message.content
 
         # image function #
         raise NotImplementedError(f'Image function is not implemented yet')
@@ -109,6 +124,6 @@ class GPTService(BaseService):
 
 
 if __name__ == '__main__':
-    print(GPTService.process_query(['chat'], {'user_id': 'test'}))
-    print(GPTService.process_query(['chat', 'help'], {'user_id': 'test'}))
-    print(GPTService.process_query(['chat', 'who', 'am', 'i'], {'user_id': 'test'}))
+    print(GPTService.process_query(['chat'], 'test'))
+    print(GPTService.process_query(['chat', 'help'], 'test'))
+    print(GPTService.process_query(['chat', 'who', 'am', 'i'], 'test'))
