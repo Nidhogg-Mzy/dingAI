6efaf49ec97ce4ec94768522d7ab218c12d9a826
enor2017 <enor2017@163.com>
2023-05-17 05:49:51 -0400

    Update: done with skeleton of all functions supported in gpt

--- services/gpt.py
+++ services/gpt.py
@@ -5,7 +5,7 @@ from typing import List, Optional
 
 import openai
 
-from base_service import BaseService
+from .base_service import BaseService
 
 
 class GPTService(BaseService):
@@ -22,6 +22,7 @@ class GPTService(BaseService):
     _CACHE_FOLDER = None
     _CHAT_ENABLED = None
     _CHAT_MODEL = None
+    _MAX_TOKEN = None
     _IMAGE_ENABLED = None
 
     @staticmethod
@@ -31,15 +32,18 @@ class GPTService(BaseService):
             return None
 
         openai_configs = configs['openai']
+        root_dir = configs['project']['root_path']
         # general
         GPTService._ENABLED = openai_configs.getboolean('enabled')
         GPTService._API_KEY = openai_configs['api_key']
+        # TODO: concat path with ${root_path}/services/${cache_folder}
         GPTService._CACHE_FOLDER = openai_configs['cache_folder']
         os.makedirs(GPTService._CACHE_FOLDER, exist_ok=True)
 
         # chat function
         GPTService._CHAT_ENABLED = openai_configs.getboolean('chat_enabled')
         GPTService._CHAT_MODEL = openai_configs['chat_model']
+        GPTService._MAX_TOKEN = openai_configs['max_tokens']
 
         # image function
         GPTService._IMAGE_ENABLED = openai_configs.getboolean('image_enabled')
@@ -83,9 +87,10 @@ class GPTService(BaseService):
 
         # chat function #
         if query[0] == 'chat':
-            # check if there is a cache file (json format) for this user,
-            # if there is, retrieve the chat history, otherwise, create a new cache file
-            cache_file = f'{GPTService._CACHE_FOLDER}/{user_id}.json'
+            # always write to temp file, then depending on whether user save or discard it,
+            # either rename to a meaningful name provided by user, or delete it
+            os.makedirs(f'{GPTService._CACHE_FOLDER}/{user_id}', exist_ok=True)
+            cache_file = f'{GPTService._CACHE_FOLDER}/{user_id}/temp.json'
             try:
                 with open(cache_file, 'r') as f:
                     chat_history = json.load(f)
@@ -100,7 +105,7 @@ class GPTService(BaseService):
             response = openai.ChatCompletion.create(
                 model=GPTService._CHAT_MODEL,
                 messages=chat_history,
-                max_tokens=500      # max length of generated completion
+                max_tokens=GPTService._MAX_TOKEN
             )
             # add the response to the chat history
             chat_history.append(response.choices[0].message)
@@ -109,21 +114,64 @@ class GPTService(BaseService):
                 json.dump(chat_history, f)
             # return the response
             return response.choices[0].message.content
+        elif query[0] in ['chathistory', 'chathist']:
+            # list histories we have
+            user_folder = f'{GPTService._CACHE_FOLDER}/{user_id}'
+            os.makedirs(user_folder, exist_ok=True)     # in case user didn't have a folder
+            all_files = os.listdir(user_folder)
+            # TODO: use regex to filter all files in format (%d+)-(.*).json
+            history_files = filter(lambda x: True, all_files)
+
+        elif query[0] == 'chatload':
+            # load specific chat to temp.json
+            # don't touch original chat history unless user save it
+            if len(query) < 2:
+                return '[Error] You need to specify history number to load a chat history'
+            history_no = query[1]
+            user_folder = f'{GPTService._CACHE_FOLDER}/{user_id}'
+            os.makedirs(user_folder, exist_ok=True)  # in case user didn't have a folder
+            all_files = os.listdir(user_folder)
+            # TODO: use regex to find history in format ${history_no}-(.*).json
+            history_file = None
+            if history_file is None:
+                return '[Error] History no is not valid. If you think this is an error, please contact administrator.'
+            # history file found! load into temp.json
+            with open(history_file, 'r') as f:
+                history_content = f.read()
+            with open(f'{user_folder}/temp.json', 'w+') as f:
+                f.write(history_content)
+            # TODO: prompt success, and display last query and answer
+            raise NotImplementedError()
+
+        elif query[0] == 'chatsave':
+            # TODO: implement this
+            raise NotImplementedError()
+
+        elif query[0] == 'chatdiscard':
+            # TODO: implement this
+            raise NotImplementedError()
 
         # image function #
+        # TODO: implement image function
         raise NotImplementedError(f'Image function is not implemented yet')
 
     @staticmethod
     def get_help() -> str:
         return f'**GPTService is a service that uses ChatGPT-3.5 API provided by OpenAI to generate text.**\n' \
                f'[Usage]\n' \
-               f'- chat <message>\n' \
-               f'- image <message> (not available)\n' \
+               f'- `chat <message>`: send <message> to GPT, and get the response.\n' \
+               f'- `chathistory` or `chathist`: list the history chats with GPT.\n' \
+               f'- `chatload <history no>`: continue the corresponding history chat, <history no> can be ' \
+               f'obtained by `chathistory`. *Notice that current chat session will be discarded.*\n' \
+               f'- `chatsave <name>`: Quit current GPT session, and **save** chats history with given name.\n' \
+               f'- `chatdiscard`: (Encouraged) Quit current GPT session, and **discard** chats history.\n' \
                f'[Example]\n' \
                f'- chat who are you\n'
 
 
 if __name__ == '__main__':
+    # TODO: load config here
+    GPTService.load_config()
     print(GPTService.process_query(['chat'], 'test'))
     print(GPTService.process_query(['chat', 'help'], 'test'))
     print(GPTService.process_query(['chat', 'who', 'am', 'i'], 'test'))
