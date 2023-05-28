bbefa35f31f5ac657adfba6028e0e3876c0cc794
Jianmeng Liu <36726166+enor2017@users.noreply.github.com>
2023-05-28 10:20:17 +0800

    Refactor Services and Basic Func of GPTService (#17)
    * Update: add base service and gpt config file
    
    * Update: chatGPT basic functions done
    
    * Update: add docs in chatGPT func
    
    * Doc: gpt help message uses markdown syntax
    
    * Update: base service
    
    * Refactor: gpt service implements load_config
    
    * Update: done with skeleton of all functions supported in gpt
    
    * Update: add SERVICES_MAP in services module.
    
    * Fix: typo in service map dict
    
    * Test: basic chat func of GPT
    
    * Fix: basic chat func of gpt
    
    * Dependency: fix version of typing
    
    * CI: create config.ini file
    
    * Fix: gpt query validation

---
+++ services/base_service.py
@@ -0,0 +1,35 @@
+from typing import List, Optional
+
+class BaseService:
+    """
+    This class defines basic functions that a service should implement.
+    All Service classes are expected to inherit this class.
+    """
+
+    @staticmethod
+    def load_config(configs: dict) -> None:
+        """
+        Load the given config to this service. This method should be called before processing
+        any query, and should guarantee to be executed only once, no matter how many times it's called.
+
+        :param configs: a dict, the complete configs loaded from config.ini
+        """
+
+    @staticmethod
+    def process_query(query: List[str], user_id: str) -> str:
+        """
+        Processes the query that user passed in, and return a reply.
+
+        :param query: a list of strings that is the query from front-end, an example can be ['ddl', 'today']
+        :param user_id: the account of the user who perform the query.
+        :return: a string that is the result of the query to be displayed to user
+        :raises ValueError: if the query or user_id is invalid, which indicates a program bug.
+        """
+        raise NotImplementedError
+
+    @staticmethod
+    def get_help() -> str:
+        """
+        Returns the help message of the service. This function should be called in process_query.
+        """
+        raise NotImplementedError
