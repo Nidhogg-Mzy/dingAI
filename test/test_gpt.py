ba9582714ad3b873e57f3997867f57dccdf2d37b
enor2017 <enor2017@163.com>
2023-05-29 15:54:40 +0800

    Fix: change test_gpt userid

--- test/test_gpt.py
+++ test/test_gpt.py
@@ -1,4 +1,6 @@
 import unittest
+import os
+import shutil   # for rmtree
 import configparser
 from services.gpt import GPTService
 
@@ -16,6 +18,13 @@ class GPTTests(unittest.TestCase):
             self.skipTest('GPTService/Chat Service is not enabled in config.ini')
 
     def tearDown(self) -> None:
+        # delete two test cache folder
+        cache_folder_1 = f'{GPTService._CACHE_FOLDER}/test1'
+        cache_folder_2 = f'{GPTService._CACHE_FOLDER}/test2'
+        # delete the folder and its content
+        shutil.rmtree(cache_folder_1, ignore_errors=True)
+        shutil.rmtree(cache_folder_2, ignore_errors=True)
+
         # reset the initialized flag
         GPTService._initialized = False
 
@@ -26,24 +35,24 @@ class GPTTests(unittest.TestCase):
         help_msg = GPTService.get_help()
         self.assertTrue('[Usage]' in help_msg)
 
-        resp = GPTService.process_query(['chat', 'help'], 'test')
+        resp = GPTService.process_query(['chat', 'help'], 'test1')
         self.assertTrue('[Usage]' in resp)
 
     def test_invalid_query(self):
         with self.assertRaises(ValueError):
-            GPTService.process_query(['invalid'], 'test')
+            GPTService.process_query(['invalid'], 'test1')
         with self.assertRaises(ValueError):
-            GPTService.process_query([], 'test')
+            GPTService.process_query([], 'test1')
 
-        resp = GPTService.process_query(['chat'], 'test')
+        resp = GPTService.process_query(['chat'], 'test1')
         self.assertTrue(resp.startswith('[Error] Your query is not complete.'))
 
     def test_basic_chat(self):
-        resp = GPTService.process_query(['chat', 'Who', 'won', 'the', 'world', 'series', 'in', '2020?'], 'test')
+        resp = GPTService.process_query(['chat', 'Who', 'won', 'the', 'world', 'series', 'in', '2020?'], 'test1')
         self.assertGreater(len(resp), 0)
         self.assertTrue('Los Angeles Dodgers' in resp)
 
-        resp = GPTService.process_query(['chat', 'Where', 'was', 'it', 'played?'], 'test')
+        resp = GPTService.process_query(['chat', 'Where', 'was', 'it', 'played?'], 'test1')
         self.assertGreater(len(resp), 0)
         self.assertTrue('Arlington' in resp, 'GPT should have memory to answer this question')
 
