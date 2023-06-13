aa60cf3043d662ffd36d1c7dd2e7c5ac2dead0a7
MENG Zeyuan <mengzeyuan64@gmail.com>
2023-06-13 14:27:04 +0800

    add get_waitlist to class App

--- app.py
+++ app.py
@@ -85,32 +85,32 @@ class App:
             return "抱歉，我没有理解您的意思，请换一个问题试试？"
         return answer
 
+    @staticmethod
+    def get_waitlist(qq: str) -> str:
+        url = "https://w5.ab.ust.hk/msapi/sis/stdt_class_enrl/%7BstdtID%7D"
+        payload = {}
 
-def get_waitlist(qq: str) -> str:
-    url = "https://w5.ab.ust.hk/msapi/sis/stdt_class_enrl/%7BstdtID%7D"
-    payload = {}
+        def get_header(qq: str) -> dict:
+            with open("test.config", "r") as f:
+                all_configs = json.load(f)["hkust_oauth_token"]
+            return {
+                'Authorization': f'Bearer {all_configs[qq]}',
+                'Cookie': 'language=en-US'
+            }
 
-    def get_header(qq: str) -> dict:
-        with open("test.config", "r") as f:
-            all_configs = json.load(f)["hkust_oauth_token"]
-        return {
-            'Authorization': f'Bearer {all_configs[qq]}',
-            'Cookie': 'language=en-US'
-        }
+        headers = get_header(qq)
+        print(headers)
 
-    headers = get_header(qq)
-    print(headers)
+        response = requests.request("GET", url, headers=headers, data=payload)
 
-    response = requests.request("GET", url, headers=headers, data=payload)
+        json_response = json.loads(response.text)
+        stdInfo = json_response['stdtInfo'][0]
+        waitList = stdInfo['studentClassWaitlist']
+        info = ''
+        for c in waitList:
+            info += f'course code: {c["crseCode"]}, waitlist position: {c["waitPosition"]}\n'
 
-    json_response = json.loads(response.text)
-    stdInfo = json_response['stdtInfo'][0]
-    waitList = stdInfo['studentClassWaitlist']
-    info = ''
-    for c in waitList:
-        info += f'course code: {c["crseCode"]}, waitlist position: {c["waitPosition"]}\n'
-
-    return info
+        return info
 
 
 if __name__ == '__main__':
