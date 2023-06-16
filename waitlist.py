e32b6454134a0fb54aecd4266cd12cfaa05c583f
MENG Zeyuan <mengzeyuan64@gmail.com>
2023-06-16 23:20:52 +0800

    stored waitlist api url in config.ini

--- waitlist.py
+++ waitlist.py
@@ -1,30 +1,38 @@
 import json
+from configparser import ConfigParser
 import requests
 
-url = "https://w5.ab.ust.hk/msapi/sis/stdt_class_enrl/%7BstdtID%7D"
+config = ConfigParser()
+config.read('config.ini')
+URL = config['waitlist']['url']
 
 payload = {}
+
+
 def get_header(qq: str) -> dict:
-  all_configs = None
-  with open("test.config", "r") as f:
-    all_configs = json.load(f)
-  return {} if qq not in all_configs else \
-  {
-    'Authorization': f'Bearer {all_configs[qq]}',
-    'Cookie': 'language=en-US'
-  }
+    """
+    get header from qq account
+    """
+    all_configs = None
+    with open("test.config", "r", encoding="utf-8") as f:
+        all_configs = json.load(f)
+    return {} if qq not in all_configs else \
+        {
+            'Authorization': f'Bearer {all_configs[qq]}',
+            'Cookie': 'language=en-US'
+        }
+
 
 if __name__ == '__main__':
-  headers = get_header("3429582673")
+    headers = get_header("3429582673")
 
-  response = requests.request("GET", url, headers=headers, data=payload)
+    response = requests.request("GET", URL, headers=headers, data=payload, timeout=20)
 
-  json_response = json.loads(response.text)
-  stdInfo = json_response['stdtInfo'][0]
-  waitList = stdInfo['studentClassWaitlist']
-  info = ''
-  for c in waitList:
-    info += f'course code: {c["crseCode"]}, waitlist position: {c["waitPosition"]}\n'
+    json_response = json.loads(response.text)
+    stdInfo = json_response['stdtInfo'][0]
+    waitList = stdInfo['studentClassWaitlist']
+    INFO = ''
+    for c in waitList:
+        INFO += f'course code: {c["crseCode"]}, waitlist position: {c["waitPosition"]}\n'
 
-  print(info)
-
+    print(INFO)
