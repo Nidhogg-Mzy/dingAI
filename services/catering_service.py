26acebcb13a37f7cb4f6e2334eb64295b2caa040
MENG Zeyuan <mengzeyuan64@gmail.com>
2023-07-04 14:02:39 +0800

    fixed some bugs

--- services/catering_service.py
+++ services/catering_service.py
@@ -1,3 +1,4 @@
+from json import JSONDecodeError
 from typing import List
 import requests
 import json
@@ -24,7 +25,7 @@ class CateringService(BaseService):
     def get_help() -> str:
         return '**CateringService is a service that uses HKUST catering API to provide the precise open time of each catering facility.** \n\n ' \
                '**[Basic Usage]** \n ' \
-               '- *catering/cater/Cater* <date>: get the open time of the catering facilities in HKUST on the specific date.\n' \
+               '- *catering/cater/Cater* [date]: get the open time of the catering facilities in HKUST on the specific date.\n' \
                '- *catering/cater/Cater* today: get today\'s open time of the catering facilities in HKUST.\n' \
                '- *catering/cater/Cater* tmr/tomorrow: get tomorrow\'s open time of the catering facilities in HKUST.\n' \
 
@@ -33,15 +34,19 @@ class CateringService(BaseService):
     def get_data(date):
         url = f"{CateringService.url}{date}"
         response = requests.request("GET", url, timeout=20)
+        print(response.text)
         if response.status_code == 200:
             return_val = ''
             for keys, values, in json.loads(response.text).items():
                 return_val += f'- {keys}: {values}\n'
             return return_val
         else:
-            return json.loads(response.text)['error']
+            try:
+                return json.loads(response.text)['error']
+            except (KeyError, JSONDecodeError):
+                return 'Unknown Error: ' + response.text
 
 
 if __name__ == '__main__':
     CateringService.url = 'http://localhost:60001/catering/'
-    print(CateringService.process_query(['catering'], user_id='qlrj2'))
+    print(CateringService.process_query(['catering', '88/888888'], user_id='qlrj2'))
