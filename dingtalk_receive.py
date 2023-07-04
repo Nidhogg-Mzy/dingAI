d5ce132fb60026311d6f390dcdd0d45e862a274b
Jianmeng Liu <36726166+enor2017@users.noreply.github.com>
2023-07-04 11:40:53 +0800

    Update: use new send message api in dingtalk (#22)
    * Update: add AccessToken in Dingtalk service
    
    * Update: use new API to send message
    
    * Update: use new API to reply message
    
    * Update: use auth_info to separate private and group msg
    
    * Update: re-write handle_reply with new auth_info
    
    * Update: return response after send msg
    
    * Fix: wrong auth_info, missed robotCode for group msg
    
    * Refactor: send feedcard msg, and modify medium service accordingly
    
    * Update: add misfire grace time in medium service

--- dingtalk_receive.py
+++ dingtalk_receive.py
@@ -6,6 +6,8 @@ import random
 import logging
 import configparser
 import requests
+from datetime import datetime, timedelta
+from typing import Literal, Optional, Union
 from flask import Flask, jsonify, make_response, request
 from services import SERVICES_MAP
 from services.medium_daily_push import MediumService
@@ -20,6 +22,56 @@ class Receive:
     app = Flask(__name__)
     configs = configparser.ConfigParser()
     configs.read('config.ini')
+
+    class _AccessToken:
+        """
+        This class is used to get the access token of the robot.
+        """
+        app_key: str
+        app_secret: str
+        access_token: Optional[str]
+        expire_time: datetime
+
+        @classmethod    # use class method for better class variable reference
+        def init_token(cls, app_key: str, app_secret: str):
+            cls.app_key = app_key
+            cls.app_secret = app_secret
+            cls.access_token = None
+            cls.expire_time = datetime.now()
+
+        @classmethod
+        def get_access_token(cls) -> str:
+            """
+            Returns the `x-acs-dingtalk-access-token` of robot.
+            If current token is invalid, a new token will be fetched using AppKey and AppSecret.
+            doc: https://open.dingtalk.com/document/orgapp/obtain-the-access_token-of-an-internal-app?spm=ding_open_doc.document.0.0.263e1563MtqouX
+            
+            :return: the access token of the robot
+            :raises ValueError: if AppKey or AppSecret is not set, or failed to get access token
+                using the given AppKey and AppSecret
+            """# noqa
+            if cls.app_key is None or cls.app_secret is None:
+                raise ValueError('Please call init_token first.')
+
+            if cls.access_token is None or cls.expire_time is None or cls.expire_time < datetime.now():
+                url = f'https://api.dingtalk.com/v1.0/oauth2/accessToken'
+                payload = {
+                    'appKey': cls.app_key,
+                    'appSecret': cls.app_secret
+                }
+                response = requests.post(url, json=payload)
+                if response.status_code == 200:
+                    response_json = response.json()
+                    cls.access_token = response_json['accessToken']
+                    cls.expire_time = datetime.now() + timedelta(seconds=int(response_json['expireIn'])) \
+                                                     + timedelta(seconds=-60)   # some buffer time
+                else:
+                    raise ValueError(f'Failed to get access token: {response.json()}')
+            return cls.access_token
+
+    # this variable is strictly private for security reason
+    __access_token = _AccessToken()
+    __access_token.init_token(configs['dingtalk']['app_key'], configs['dingtalk']['app_secret'])
 
     @staticmethod
     def verify_sign(header: dict) -> bool:
@@ -44,44 +96,81 @@ class Receive:
         return sign == header['sign']
 
     @staticmethod
-    def parse_msg(msgtype: str, msg: str, user_id: str, is_at_all: bool = False):
+    def generate_msg_param(msg_key: Literal["sampleText", "sampleMarkdown"], msg: str) -> str:
+        # TODO: user_id and at_all function?
         """
-        format of text message sent
-        {
-            "at": {
-                "atMobiles":[
-                    "180xxxxxx"
-                ],
-                "atUserIds":[
-                    "user123"
-                ],
-                "isAtAll": false
-            },
-            "text": {
-                "content":"我就是我, @XXX 是不一样的烟火"
-            },
-            "msgtype":"text"
-        }
+        This function generates the msg_param for sending message.
+
+        doc: https://open.dingtalk.com/document/isvapp/bots-send-group-chat-messages
         """
-        data = {}
-        if msgtype in ['text', 'markdown']:
-            data = {
-                "msgtype": "markdown",
-                "markdown": {
-                    "title": "DingAI消息",
-                    "text": msg
-                },
-                "at": {
-                    "atUserIds": [
-                        user_id
-                    ],
-                    "isAtAll": is_at_all
-                }
+        msg_param = {}
+        if msg_key in ['sampleText', 'sampleMarkdown']:
+            msg_param = {
+                "title": "DingAI消息",
+                "text": msg,
             }
-        return data
+        # TODO: more supported msg_key
+        return str(msg_param)
 
     @staticmethod
-    def send_feedcard_msg(titles: list, message_urls: list, image_urls: list):
+    def send_msg(msg: str,
+                 msg_key: Literal["sampleText", "sampleMarkdown"] = 'sampleMarkdown',
+                 auth_info: dict[str, Union[str, list[str]]] = {}) -> str:
+        """
+        This function sends a message to the user, either in private or group chat.
+        
+        :param msg: the message to be sent
+        :param msg_key: the type of the message, default is 'sampleMarkdown'
+        :param auth_info: the auth info. 
+            For a private chat, it should be 
+            <code>{'robotCode': 'robot_code', 'userIds': ['user_id1', 'user_id2']}</code>.
+            For a group chat, it should be 
+            <code>{'robotCode': 'robot_code', 'openConversationId': 'open_conversation_id'}</code>.
+
+        doc: https://open.dingtalk.com/document/isvapp/send-single-chat-messages-in-bulk
+        doc: https://open.dingtalk.com/document/isvapp/bots-send-group-chat-messages
+        {
+          "msgParam" : "String",
+          "msgKey" : "String",
+          # group chat
+          "openConversationId" : "String",
+          "robotCode" : "String",
+          # private chat
+          "userIds" : Array[String],
+          "robotCode" : "String",
+        }
+        """  # noqa
+        base_url = 'https://api.dingtalk.com/v1.0/robot/{}'
+        is_group_chat: bool = 'openConversationId' in auth_info
+        url = base_url.format('groupMessages/send' if is_group_chat else 'oToMessages/batchSend')
+        payload = {
+            "msgParam": Receive.generate_msg_param(msg_key, msg),
+            "msgKey": msg_key,
+            **auth_info
+        }
+        headers = {
+            'Content-Type': 'application/json',
+            'x-acs-dingtalk-access-token': Receive.__access_token.get_access_token()
+        }
+        response = requests.post(url, json=payload, headers=headers)
+        return response.json()
+
+    @staticmethod
+    def send_feedcard_msg(titles: list[str], message_urls: list[str],
+                          image_urls: list[str], webhooks: list[str]) -> dict[str, str]:
+        """
+        This function sends a feedcard message to the group chat.
+        Notice that Webbook must be used in order to send feedcard-type messages.
+
+        doc: https://open.dingtalk.com/document/orgapp/robot-message-types-and-data-format
+
+        :param titles: a list of titles of the feedcard
+        :param message_urls: a list of message urls of the feedcard
+        :param image_urls: a list of image urls of the feedcard
+        :param webhooks: a list of webhooks of all the groups that the message will be sent to
+
+        :return: a dict of responses of all the webhooks, key is the webhook, value is the response
+        """
         result = []
         headers = {'Content-Type': 'application/json'}
         for title, message_url, image_url in zip(titles, message_urls, image_urls):
@@ -100,22 +189,30 @@ class Receive:
                 "links": links
             }
         }
-        url = Receive.configs['dingtalk']['url']
-        print(requests.post(url, json=data, headers=headers, timeout=30).text)
+        responses = {}
+        for webhook in webhooks:
+            resp = requests.post(webhook, json=data, headers=headers, timeout=30)
+            responses['webhook'] = resp.json()
+
+        print(f'responses: {responses}')
+        return responses
 
     @staticmethod
-    def rev_msg():  # json or None
-        body = json.loads(request.data.decode('utf-8'))
-        parsed_msg = Receive.parse_body(body)
-        return parsed_msg
-
-    @staticmethod
-    def parse_body(body) -> dict:
-        parsed_msg = {'message_type': body['msgtype'],
-                      'conversation_type': 'private' if body['conversationType'] == 1 else 'group',
-                      'msg': body['text']['content'], 'senderId': body['senderStaffId'],
-                      'sender_nick': body['senderNick']}
-        return parsed_msg
+    def parse_body(body) -> (dict, dict):
+        parsed_msg = {
+            'conversationId': body['conversationId'],
+            'message_type': body['msgtype'],
+            'conversation_type': 'private' if body['conversationType'] == '1' else 'group',
+            'msg': body['text']['content'],
+            'senderId': body['senderStaffId'],
+            'sender_nick': body['senderNick']
+        }
+        auth_info = {'robotCode': body['robotCode']}
+        if parsed_msg['conversation_type'] == 'group':
+            auth_info['openConversationId'] = body['conversationId']
+        else:
+            auth_info['userIds'] = [parsed_msg['senderId']]
+        return parsed_msg, auth_info
 
     @staticmethod
     @app.route('/', methods=['POST'])
@@ -123,31 +220,37 @@ class Receive:
         """
         All private/group message processing are done here.
         """
-        received = Receive.rev_msg()
-        return Receive.handle_reply(received)
+        body = json.loads(request.data.decode('utf-8'))
+        received, auth_info = Receive.parse_body(body)
+        return Receive.handle_reply(received, auth_info)
 
     @staticmethod
-    def handle_reply(received):
-        user_id = received['senderId']
-        message_parts = received['msg'].strip().split(' ')
+    def handle_reply(received: dict, auth_info: dict) -> str:
+        """
+        Given a received message (parsed by `parse_body`), this function will reply the message
+        by calling DingTalk API.
+        """
+        user_id: str = received['senderId']
+        message_parts: list[str] = received['msg'].strip().split(' ')
+
         if len(message_parts) < 1:
-            return_msg = Receive.parse_msg(msgtype='markdown', user_id=user_id, msg='蛤?')
+            return Receive.send_msg(msg='蛤?', msg_key='sampleMarkdown', auth_info=auth_info)
         elif message_parts[0] == '在吗':
-            return_msg = Receive.parse_msg(msgtype='markdown', user_id=user_id,
-                                           msg=Receive.reply_msg[random.randint(0, len(Receive.reply_msg) - 1)])
+            return Receive.send_msg(msg=Receive.reply_msg[random.randint(0, len(Receive.reply_msg) - 1)],
+                                    msg_key='sampleMarkdown',
+                                    auth_info=auth_info)
+        try:
+            service = SERVICES_MAP[message_parts[0]]
+        except KeyError:
+            return Receive.send_msg(msg='We currently do not support this kind of service',
+                                    msg_key='sampleMarkdown',
+                                    auth_info=auth_info)
         else:
-            try:
-                service = SERVICES_MAP[message_parts[0]]
-            except KeyError:
-                return_msg = Receive.parse_msg(msgtype='markdown', user_id=user_id,
-                                               msg="We currently do not support this kind of service")
-            else:
-                service.load_config(Receive.configs)
-                return_msg = Receive.parse_msg(msgtype='markdown', user_id=user_id,
-                                               msg=service.process_query(message_parts, user_id))
-        response = make_response(jsonify(return_msg))
-        response.headers['Content-Type'] = 'application/json'
-        return response
+            service.load_config(Receive.configs)
+            # TODO: support other types of messages
+            return Receive.send_msg(msg=service.process_query(message_parts, user_id),
+                                    msg_key='sampleMarkdown',
+                                    auth_info=auth_info)
 
 
 if __name__ == '__main__':
